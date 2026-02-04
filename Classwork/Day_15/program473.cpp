58f20df3289d8827102570278fa1cf3ea8791952
Sakshi Ravindra Darandale <sakshidarandale8@gmail.com>
2026-02-04 15:49:33 +0530

    Add files via upload

---
+++ Classwork/Day_15/program473.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+class ArrayX
+{
+    public:
+        int *Arr; 
+        int iSize=0;
+        
+        ArrayX(int no)
+        {
+            cout<<"Inside Constructor\n";
+            iSize=no;
+            Arr=new int[iSize];
+        }    
+        ~ArrayX()
+        {
+            cout<<"Inside Destructor\n";
+            delete [] Arr;
+        }  
+        
+        void Accept()
+        {
+            int iCnt =0;
+            
+            cout<<"Enter the elements : \n";
+            
+            for(iCnt=0;iCnt<iSize;iCnt++)
+            {
+                cin>>Arr[iCnt];
+            }
+        }
+        
+        void Display()
+        {
+            int iCnt =0;
+            cout<<"Elements of the array are : \n";
+            
+            for(iCnt=0;iCnt<iSize;iCnt++)
+            {
+                cout<<Arr[iCnt]<<"\t";
+            }
+            cout<<"\n";
+        }
+};
+
+int main()
+{
+    int iValue=0;
+    
+    cout<<"Enter the number of elements : \n";
+    cin>>iValue;
+    
+    //Step 1: Allocate the memory
+    ArrayX *aobj=new ArrayX(iValue);   //dynamic
+    
+    //Strp 2:Use the memory
+    aobj->Accept();
+    aobj->Display();
+    
+    //Step 3: Deallocate the memory
+    delete aobj;
+    
+    return 0;
+}
